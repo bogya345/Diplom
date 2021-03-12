6e97d7b3fd4ab3993687abbd934584b0940da82b
bogya345 <kkedrovich@gmail.com>
2021-03-12 21:38:05 +0300

    testing part

---
+++ diploms/WebBRS-testing/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("WebBRS-testing")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("HP Inc.")]
+[assembly: AssemblyProduct("WebBRS-testing")]
+[assembly: AssemblyCopyright("Copyright © HP Inc. 2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("9ab804f5-900e-4f3e-970d-dab32391d005")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ diploms/WebBRS-testing/UI/Group.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using System.Windows.Forms;
+
+using OpenQA.Selenium.Chrome;
+
+namespace WebBRS_testing.UI
+{
+    /// <summary>
+    /// Сводное описание для Group
+    /// </summary>
+    [TestClass]
+    public class Group
+    {
+        public Group()
+        {
+            //
+            // TODO: добавьте здесь логику конструктора
+            //
+        }
+
+        private TestContext testContextInstance;
+
+        /// <summary>
+        ///Получает или устанавливает контекст теста, в котором предоставляются
+        ///сведения о текущем тестовом запуске и обеспечивается его функциональность.
+        ///</summary>
+        public TestContext TestContext
+        {
+            get
+            {
+                return testContextInstance;
+            }
+            set
+            {
+                testContextInstance = value;
+            }
+        }
+
+        #region Дополнительные атрибуты тестирования
+        //
+        // При написании тестов можно использовать следующие дополнительные атрибуты:
+        //
+        // ClassInitialize используется для выполнения кода до запуска первого теста в классе
+        // [ClassInitialize()]
+        // public static void MyClassInitialize(TestContext testContext) { }
+        //
+        // ClassCleanup используется для выполнения кода после завершения работы всех тестов в классе
+        // [ClassCleanup()]
+        // public static void MyClassCleanup() { }
+        //
+        // TestInitialize используется для выполнения кода перед запуском каждого теста 
+        // [TestInitialize()]
+        // public void MyTestInitialize() { }
+        //
+        // TestCleanup используется для выполнения кода после завершения каждого теста
+        // [TestCleanup()]
+        // public void MyTestCleanup() { }
+        //
+        #endregion
+
+        [TestMethod]
+        public void TestMethod1()
+        {
+            ChromeDriver driver = new ChromeDriver(@"D:/Profile/#_drivers");
+            driver.Url = "http://localhost:4200/";
+            var h2 = driver.FindElementByTagName("h2");
+
+            MessageBox.Show(h2.Text);
+        }
+    }
+}
