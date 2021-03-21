1e492fbcea487760ab4fd5ffabb146f0ea9fc516
semen.sychevforedu <semen.sychevforedu@gmail.com>
2021-03-21 10:23:45 +0300

    ViewsOwls

--- diploms/WebBRS/ClientApp/src/app/attedance-table/attedance-table.component.ts
+++ diploms/WebBRS/ClientApp/src/app/attedance-table/attedance-table.component.ts
@@ -1,15 +1,31 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Inject } from '@angular/core';
 
+import { attedanceTable_HttpService } from './http.serviceAttedanceTable';
+import { HttpClient, HttpRequest, HttpResponse, HttpEventType } from '@angular/common/http';
 @Component({
   selector: 'app-attedance-table',
   templateUrl: './attedance-table.component.html',
   styleUrls: ['./attedance-table.component.css']
 })
 export class AttedanceTableComponent implements OnInit {
+  public http: attedanceTable_HttpService;
 
+  public groups: Group[];
+  public students: Student[];
+  public lecturer: Lecturer;
+  public exactClassForLecturerClass: ExactClassForLecturerClass;
   constructor() { }
 
   ngOnInit() {
+    this.http.getAttedanceForLecturerClasses()
+      .subscribe(result => {
+        this.exactClassForLecturerClass = result;
+        console.log('result/constructor', result);
+
+      }, error => {
+        console.log('error/constructor', error);
+      }
+      );
   }
 
 
